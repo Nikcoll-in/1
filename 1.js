10283a47ecf378e141c7691bc46b893de25b7eec
Ruslan Nykolyn <x995368873@gmail.com>
2022-09-06 21:07:36 +0200

    T

--- 1.js
+++ 1.js
@@ -1,4 +1,4 @@
 // 1. Дана строка написана вся з маленьких літер. Зробити нову строку, де всі літери великі.
-let string = "ok google!!!";
-let resul = string.toUpperCase();
-alert(resul);
+var string = "ok google";
+var resul = string.toUpperCase();
+console.log(resul);
