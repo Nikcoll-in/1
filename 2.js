d75b381594143e2d423b52472e5aab03840c506d
Ruslan Nykolyn <x995368873@gmail.com>
2022-09-07 21:47:09 +0200

    final

--- 2.js
+++ 2.js
@@ -1,10 +1,10 @@
 // 2. Дано число. Знайти суму цифр даного числа.       ПЕРЕПИСАТИ В ЦИКЛІ
 function getSum(str) {
-    var sum = 0;
-    for (var i = 0; i < str.length; i++) {
-        sum = sum + parseInt(str[i]);
-    }
-    return sum;
+  var sum = 0;
+  for (var i = 0; i < str.length; i++) {
+    sum = sum + parseInt(str[i]);
+  }
+  return sum;
 }
-var str = "33";
+var str = "333";
 console.log(getSum(str));
