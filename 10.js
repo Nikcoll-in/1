8a3d8956862a1eaf2ea733383f0badecedcf6fdb
Ruslan Nykolyn <x995368873@gmail.com>
2022-09-14 11:58:36 +0200

    Final

---
+++ 10.js
@@ -0,0 +1,7 @@
+var x = 5;
+try {
+    x = x5 / 0;
+}
+catch (err) {
+    console.log("Error (5/0)");
+}
---
+++ 10.ts
@@ -0,0 +1,6 @@
+let x = 5;
+try {
+  x = x5 / 0;
+} catch (err) {
+  console.log("Error (5/0)");
+}
